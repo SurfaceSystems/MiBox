452e737b1de229bcdc7f7f3718ff91486f336840
SurfaceSystems <116438025+SurfaceSystems@users.noreply.github.com>
2023-04-10 13:24:49 +0200

    Add files via upload

--- src/com/game/verone/GameMotor.java
+++ src/com/game/verone/GameMotor.java
@@ -1,8 +1,15 @@
 package com.game.verone;
 
+import com.sun.istack.internal.NotNull;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Random;
 
 
 class GameMotor extends JFrame {
@@ -10,15 +17,31 @@ class GameMotor extends JFrame {
     private static int x = 0, y = 0, item = 0;
     private static String chatText = "<html>CLIENT> Game MiBox x_1.0 Started<br></html>";
 
-    GameMotor() {
+    private ImageIcon getImageIconFromURL(@NotNull String url) throws IOException {
+        URL urlconnection = new URL(url);
+        BufferedImage c = ImageIO.read(urlconnection);
+        ImageIcon image = new ImageIcon(c);
+        return image;
+    }
+
+
+
+
+    GameMotor() throws IOException {
         JFrame window = new JFrame();
         window.setTitle("MiBox");
         JPanel bg = new JPanel();
         JLabel info = new JLabel("X: 0 Y: 0  Item: 0 mibox:null");
         JLabel person = new JLabel();
+        JLabel cursor = new JLabel();
+        cursor.setIcon(getImageIconFromURL("https://raw.githubusercontent.com/SurfaceSystems/MiBox/main/cursor.png"));
+        cursor.setBounds(0,0,40,40);
 
-
-        person.setIcon(new ImageIcon("person.png"));
+        try {
+            person.setIcon(getImageIconFromURL("https://raw.githubusercontent.com/SurfaceSystems/MiBox/main/person.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         person.setBounds(x,y,40,40);
 
         info.setVisible(false);
@@ -152,11 +175,23 @@ class GameMotor extends JFrame {
                 item = NTcounter.checkLoop(0,2,item);
             }
             if(item == 0) {
-                person.setIcon(new ImageIcon("person.png"));
+                try {
+                    person.setIcon(getImageIconFromURL("https://raw.githubusercontent.com/SurfaceSystems/MiBox/main/person.png"));
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
             } else if(item == 1) {
-                person.setIcon(new ImageIcon("personstick.png"));
+                try {
+                    person.setIcon(getImageIconFromURL("https://raw.githubusercontent.com/SurfaceSystems/MiBox/main/personstick.png"));
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
             } else if(item == 2) {
-                person.setIcon(new ImageIcon("personsword.png"));
+                try {
+                    person.setIcon(getImageIconFromURL("https://raw.githubusercontent.com/SurfaceSystems/MiBox/main/personsword.png"));
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
             }
             if(item == 0) {
                 info.setText("X:" + x + " Y:" + y + "  Item: " + item + "  mibox:null");
@@ -169,14 +204,115 @@ class GameMotor extends JFrame {
 
 
 
+        Random randomx = new Random();
+        int randx = randomx.nextInt(26) * 20;
+        Random randomy = new Random();
+        int randy = randomy.nextInt(26) * 20;
+        JLabel bed = new JLabel();
+        bed.setBounds(randx,randy, 40,40);
+        try {
+            bed.setIcon(getImageIconFromURL("https://raw.githubusercontent.com/SurfaceSystems/MiBox/main/bed.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
 
         window.setBounds(0,0,500,500);
         bg.setBackground(new Color(13, 255, 20));
         bg.setBounds(0,0,20,20);
+        try {
+            window.setIconImage(getImageIconFromURL("https://raw.githubusercontent.com/SurfaceSystems/MiBox/main/person.png").getImage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        window.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if(item == 0) {
+                    try {
+                        person.setIcon(getImageIconFromURL("https://raw.githubusercontent.com/SurfaceSystems/MiBox/main/personuse.png"));
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                } else if(item == 2) {
+                    try {
+                        person.setIcon(getImageIconFromURL("https://raw.githubusercontent.com/SurfaceSystems/MiBox/main/personsworduse.png"));
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                } else if(item == 1) {
+                    try {
+                        person.setIcon(getImageIconFromURL("https://raw.githubusercontent.com/SurfaceSystems/MiBox/main/personstickuse.png"));
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if(item == 0) {
+                try {
+                    person.setIcon(getImageIconFromURL("https://raw.githubusercontent.com/SurfaceSystems/MiBox/main/person.png"));
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            } else if(item == 2) {
+                try {
+                    person.setIcon(getImageIconFromURL("https://raw.githubusercontent.com/SurfaceSystems/MiBox/main/personsword.png"));
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            } else if(item == 1) {
+                try {
+                    person.setIcon(getImageIconFromURL("https://raw.githubusercontent.com/SurfaceSystems/MiBox/main/personstick.png"));
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+
+        });
+
+        window.addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                cursor.setBounds(e.getX()-10,e.getY()-30,40,40);
+            }
+        });
+
 
         window.add(person);
         window.add(info);
+        window.add(bed);
+        window.add(cursor);
         window.add(bg);
+        window.setCursor( window.getToolkit().createCustomCursor(
+                new BufferedImage( 1, 1, BufferedImage.TYPE_INT_ARGB ),
+                new Point(),
+                null ) );
         window.setVisible(true);
 
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
--- src/com/game/verone/Main.java
+++ src/com/game/verone/Main.java
@@ -1,9 +1,33 @@
 package com.game.verone;
 
 
+import javax.swing.*;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("CLIENT> Game MiBox x_1.0 started");
-        new GameMotor();
+        int conection = 0;
+        try {
+            URL url = new URL("http://www.google.com");
+            URLConnection connection = url.openConnection();
+            connection.connect();
+        } catch (MalformedURLException e) {
+            conection = 1;
+        } catch (IOException e) {
+            conection = 1;
+        }
+        if(conection == 0) {
+            System.out.println("CLIENT> Game MiBox x_1.0 started");
+            try {
+                new GameMotor();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            JOptionPane.showConfirmDialog(null, "The game requires internet connection to load sprites, connect to internet and try again", "Internet error", JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
